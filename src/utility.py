30cc0bae121fd5411d37be6ff4629dfb35225603
s3488631 <s3488631@student.rmit.edu.au>
2019-04-30 12:22:25 +1000

    Create utility.py

---
+++ src/utility.py
@@ -0,0 +1,18 @@
+import hashlib
+import binascii
+import os
+
+
+class Utility:
+
+    def verify_user(self, username):
+        pass
+
+    def verify_email(self, email):
+        pass
+
+    def hash_pass(self, password):
+        pass
+
+    def verify_pass(self, table_password, provided_password):
+        pass
