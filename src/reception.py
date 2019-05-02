dcd1ed17abf3b33dbeab957d09f9151b9c2d0854
Dominique McDonnell <dominic.d.mcdonnell@gmail.com>
2019-05-02 21:44:15 +1000

    Added sockets

--- src/reception.py
+++ src/reception.py
@@ -2,13 +2,15 @@ from .console import Console
 from .console_state import ConsoleState
 from .sqlite_db_interface import SqliteDbInterface
 from .utility import Utility
+from .socket import Socket
 
 
-class Reception():
-    def __init__(self):
-        self.db_interface = SqliteDbInterface()
-        self.utility = Utility(self.db_interface)
-        self.console = Console(self.utility)
+class Reception:
+    def __init__(self, socket_connection):
+        db_interface = SqliteDbInterface()
+        socket = Socket(socket_connection)
+        utility = Utility(db_interface, socket)
+        self.console = Console(utility)
 
         # add states
 
@@ -17,5 +19,5 @@ class Reception():
 
 
 if __name__ == '__main__':
-    main = Reception()
+    main = Reception('127.0.0.0:5000')
     main.run()
