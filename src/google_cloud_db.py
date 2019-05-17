386cc82ad376c3c03b46df997502b73d1975a4ad
s3488631 <s3488631@student.rmit.edu.au>
2019-05-17 17:14:31 +1000

    Latest Update, can't seem to get it to work
    Going to look into the admin website functionality for now, bit stuck on this at the moment.

--- src/google_cloud_db.py
+++ src/google_cloud_db.py
@@ -1,70 +1,48 @@
 from .book import Book
-from .user import User
-#from .borrowing import Borrowing
-import MySQLdb 
+import MySQLdb
 
 
-class GoogleCloudDB:
+class GoogleCloudDb():
     """
-    this is the google database class
     """
-
-    HOST="localhost",
-    USER="user",
-    PASSWORD="123",
-    DATABASE="dbcloud"
-    
+    HOST='localhost'
+    USER='root'
+    PASSWORD='omitted'
+    DATABASE='dbcloud'
 
     def __init__(self):
-        self.connection = MySQLdb.connect(GoogleCloudDB.HOST, GoogleCloudDB.USER,
-            GoogleCloudDB.PASSWORD, GoogleCloudDB.DATABASE)
-        self.books = []
-        self.borrowings = []
+        self.connection = MySQLdb.connect(GoogleCloudDb.HOST, GoogleCloudDb.USER, GoogleCloudDb.PASSWORD, GoogleCloudDb.DATABASE)
 
-    def load_books(self):
-        """
-        loads table books from google cloud database
-        and places it into a list
-        """
+    #def connect_(self):
+     #   try:
+      #       cursor = self.connection.cursor()
+       # finally:
+        #    self.connection.close()
 
+    def search_(self, input, value):
         try:
-            with self.connection as cursor:
-                sql = "SELECT * FROM Book"
-                cursor.execute(sql)
-                (book_id, title, author, published_date) = cursor.fetchone()
-                b1 = Book(book_id, title, author, published_date)
-                self.books.insert(b1)
-
+            cursor = self.connection.cursor()
+            if input == 1:
+                sql = "SELECT * FROM Book WHERE BookID = %s"
+                for row in cursor.execute(sql, (value, )):
+                    (book_id, title, author, published_date) = row
+                    return Book(book_id=book_id,title=title,author=author,published_date=published_date)
+            elif input == 2:
+                sql = "SELECT * FROM Book WHERE Title LIKE %s"
+                for row in cursor.execute(sql, (value, )):
+                    (book_id, title, author, published_date) = row
+                    return Book(book_id=book_id,title=title,author=author,published_date=published_date)
+            elif input == 3: 
+                sql = "SELECT * FROM Book WHERE Author LIKE %s"
+                for row in cursor.execute(sql, (value, )):
+                    (book_id, title, author, published_date) = row
+                    return Book(book_id=book_id,title=title,author=author,published_date=published_date)
+            elif input == 4:
+                sql = "SELECT * FROM Book WHERE DatePublished LIKE %s"
+                for row in cursor.execute(sql, (value, )):
+                    (book_id, title, author, published_date) = row
+                    return Book(book_id=book_id,title=title,author=author,published_date=published_date)
+            else:
+                print("Something has gone terribly wrong")
         finally:
             self.connection.close()
-
-    def searh_books(self, input):
-        """
-        """
-
-        try:
-            with self.connection as cursor:
-                if input == 1:
-                    sql = "SELECT * FROM Book WHERE BookID LIKE %s"
-                    cursor.execute(sql, input)
-                    (book_id, title, author, published_date) = cursor.fetchall
-                elif input == 2:
-                    sql = "SELECT * FROM Book WHERE Title LIKE %s"
-                    cursor.execute(sql, input)
-                    (book_id, title, author, published_date) = cursor.fetchall
-                elif input == 3: 
-                    sql = "SELECT * FROM Book WHERE Author LIKE %s"
-                    cursor.execute(sql, input)
-                    (book_id, title, author, published_date) = cursor.fetchall
-                elif input == 4:
-                    sql = "SELECT * FROM Book WHERE DatePublished LIKE %s"
-                    cursor.execute(sql, input)
-                    (book_id, title, author, published_date) = cursor.fetchall
-                else:
-                    print("Something has gone terribly wrong")
-                    
-    def load_borrowings(self):
-        pass
-
-    def save_borrowing(self):
-        pass
