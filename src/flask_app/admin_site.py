2b74a7f69fddde9a8d24e66fcb7b2b9a91773ec4
s3488631 <s3488631@student.rmit.edu.au>
2019-05-20 23:42:14 +1000

    Update admin_site.py

--- src/flask_app/admin_site.py
+++ src/flask_app/admin_site.py
@@ -1,13 +1,41 @@
 from flask import Flask, Blueprint, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_wtf import FlaskForm
+from admin_form import LoginForm
 import os, requests, json
+
 
 site = Blueprint("site", __name__)
 
 @site.route("/")
+@site.route("/home")
 def index():
-    response = requests.get("http://127.0.0.1:5000/book")
-    data = json.loads(response.text)
+    return render_template("index.html")
 
-    return render_template("index.html", book = data)
+@site.route("/login")
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/home')
+    return render_template('login.html', title='Sign In', form=form)
+
+@site.route("/search")
+def book_search():
+    
+    
+    response = requests.get("http://127.0.0.1:5000/book")
+    return render_template("book_search.html")
+
+@site.route("/create")
+def book_borrow():
+    
+
+    return render_template("book_borrow")
+
+@site.route("/return")
+def book_return():
+
+    return render_template("book_return")
