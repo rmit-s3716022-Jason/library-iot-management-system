88c21891271c8f8c438e7a418b315132f3a4575d
s3488631 <s3488631@student.rmit.edu.au>
2019-05-24 00:04:53 +1000

    Update admin_site.py

--- src/flask_app/admin_site.py
+++ src/flask_app/admin_site.py
@@ -1,7 +1,8 @@
-from flask import Flask, Blueprint, request, jsonify, render_template
+from flask import Flask, Blueprint, request, jsonify, render_template, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from admin_login import LoginForm
+from admin_api import Adm
 import os, requests, json
 
 site = Blueprint("site", __name__)
@@ -12,17 +13,24 @@ def index():
 
     return render_template("index.html")
 
-@site.route("/login")
+@site.route("/login", methods=['GET', 'POST'])
+@site.route("/index.html")
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
+            form.username.data, form.remember.data))
+        return redirect('/dashboard')
     return render_template('login.html', title='Sign In', form=form)
+
+@site.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html')
+
 
 @site.route("/add", methods=['POST'])
 def add():
+    api = admin_api()
     title = request.form['Title']
     author = request.form['Author']
     date_published = request.form['Date_published']
@@ -40,7 +48,9 @@ def add():
     response = requests.post("http://127.0.0.1:5000/book", data= json.dumps(data), headers=headers)
     data = json.loads(response.text)
     
-    return render_template("index.html", data=data, title=title, author=author, date_published=date_published)
+    return render_template("dashboard.html", data=data, title=title, author=author, date_published=date_published)
+
+
 
 @site.route("/edit")
 def edit():
@@ -53,4 +63,6 @@ def remove():
 @site.route("/logout")
 def logout():
     pass
+
+
 
