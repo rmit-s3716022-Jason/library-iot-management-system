8a1cf924d8e0b8cf38190da8df3d23bae8f0c8b8
Liam Jeynes <s3544919@student.rmit.edu.au>
2019-05-23 21:17:42 +1000

    simplified code

--- src/framework/master/search_console_state.py
+++ src/framework/master/search_console_state.py
@@ -3,7 +3,6 @@ from .console_state import ConsoleState
 # Class is meant to represent the console state whilst the user is currently
 # searching for a book
 
-
 class SearchConsoleState(ConsoleState):
 
     def __init__(self, display_text):
@@ -27,21 +26,17 @@ class SearchConsoleState(ConsoleState):
         return response
 
     def handle_input(self, input_string, context):
-        if 1 <= input_string <= 4:
-            response = input('Please enter desired %s:' %
-                             self.options[input_string-1])
-            results = context.db.search(input_string, response)
+        response = input('Please enter desired %s:' %
+                            self.options[input_string-1])
+        results = context.db.search(input_string, response)
 
-            if results:
-                print("Your search has been completed.")
-                self.print_results(results)
-            else:
-                print('No results returned.')
+        if results:
+            print("Your search has been completed.")
+            self.print_results(results)
+        else:
+            print('No results returned.')
 
-            return 'main'
-
-        print("Incorrect input.")
-        return ''
+        return 'main'
 
     def print_results(self, results):
         for items in results:
