4c10a97966a608f0a8b104978b5de02e5ddeff87
s3488631 <s3488631@student.rmit.edu.au>
2019-05-24 00:06:52 +1000

    Update admin_form.py

--- src/flask_app/admin_form.py
+++ src/flask_app/admin_form.py
@@ -5,5 +5,11 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class AddForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    author = StringField('Author', validators=[DataRequired()])
+    published_date = DateTimeField('PublishedDate', format='%m/%d/%y' validators=[DataRequired()])
     submit = SubmitField('Sign In')
